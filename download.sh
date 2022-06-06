565bacf719343f123806c94cb5a9b7f41bfa40f9
Rafał Michaluk <rm360179@students.mimuw.edu.pl>
2022-06-06 15:58:35 +0200

    fix download

--- download.sh
+++ download.sh
@@ -3,5 +3,7 @@ mkdir -p data
 
 curl https://klejbenchmark.com/static/data/klej_cbd.zip --output ./data/klej_cbd.zip
 unzip ./data/klej_cbd.zip
+mv test_features.tsv data
+mv train.tsv data
 rm ./data/klej_cbd.zip
 
