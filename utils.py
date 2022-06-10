4442d5b42104d9c378b01414d80f8f0a70b5670b
misia <dominika.bankiewicz@gmail.com>
2022-06-10 18:41:39 +0200

    updated student loading

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import torch
 import numpy as np
 
-
 # czy powinniśmy zasłaniać unk token?
+from projekt_nlp.DistilHerBERT.models.distil_student import creat_student
+
+
 def create_masked_ids(data):
     mask1 = torch.rand(data.input_ids.shape) < 0.15
     mask2 = torch.tensor(~np.isin(data.input_ids.detach().cpu().numpy(), (0, 1, 2, 3)))
@@ -16,20 +18,7 @@ def get_teacher_student_tokenizer():
 
     tokenizer = AutoTokenizer.from_pretrained("allegro/herbert-base-cased")
     teacher = AutoModel.from_pretrained("allegro/herbert-base-cased")
-    student = AutoModel.from_pretrained("allegro/herbert-base-cased")
-
-    # student.encoder.layer[1] = torch.nn.Identity(768)
-    # student.encoder.layer[3] = torch.nn.Identity(768)
-    # student.encoder.layer[5] = torch.nn.Identity(768)
-    # student.encoder.layer[7] = torch.nn.Identity(768)
-    # student.encoder.layer[9] = torch.nn.Identity(768)
-    # student.encoder.layer[11] = torch.nn.Identity(768)
-
-    print(f'Number of parameters: Teacher: {count_parameters(teacher)}, Student: {count_parameters(student)},'
-          f'Student / Teacher ratio: {round(count_parameters(student) / count_parameters(teacher), 4)}.')
-
-    for params in teacher.parameters():
-        params.requires_grad = False
+    student = creat_student(teacher)
 
     return teacher, student, tokenizer
 
