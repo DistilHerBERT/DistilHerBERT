0a44d3460b792d9725c39b851d37fbcdb856d6e9
BartekK <bartekkrzepkowski@gmail.com>
2022-06-21 21:56:27 +0200

    configure optimizer to exclude some parameters from weight decay

--- trainers/utils.py
+++ trainers/utils.py
@@ -38,42 +38,16 @@ def count_parameters(model):
 
 
 def configure_optimizer(model, optim, weight_decay=1e-4, **optim_kwargs):
-    from torch import nn
-    # separate out all parameters to those that will and won't experience regularizing weight decay
-    decay = set()
-    no_decay = set()
-    # whitelist_weight_modules = (nn.Linear, nn.Conv1d, nn.Conv2d, nn.ConvTranspose2d)
-    blacklist_weight_modules = (nn.LayerNorm, nn.Embedding, nn.BatchNorm1d, nn.BatchNorm2d)
-    for mn, m in model.named_modules():
-        for pn, p in m.named_parameters():
-            fpn = '%s.%s' % (mn, pn) if mn else pn # full param name
-
-            if pn.endswith('bias'):
-                # all biases will not be decayed
-                no_decay.add(fpn)
-            elif pn.endswith('weight'):
-                if isinstance(m, blacklist_weight_modules):
-                # weights of blacklist modules will NOT be weight decayed
-                    no_decay.add(fpn)
-                else:
-                    decay.add(fpn)
-
-
-    # special case the position embedding parameter in the root GPT module as not decayed
-    # no_decay.add('pos_emb')
-
-    # validate that we considered every parameter
-    param_dict = {pn: p for pn, p in model.named_parameters()}
-    # inter_params = decay & no_decay
-    # union_params = decay | no_decay
-    # assert len(inter_params) == 0, "parameters %s made it into both decay/no_decay sets!" % (str(inter_params), )
-    # assert len(param_dict.keys() - union_params) == 0, "parameters %s were not separated into either decay/no_decay set!" \
-    #                                             % (str(param_dict.keys() - union_params), )
-    # print()
-    # create the pytorch optimizer object
-    optim_groups = [
-        {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": weight_decay},
-        {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay": 0.0},
+    decay = {pn for pn, p in model.named_parameters() if 'embeddings' in pn or 'LayerNorm' in pn}
+    optimizer_grouped_parameters = [
+        {
+            "params": [p for pn, p in model.named_parameters() if pn in decay and p.requires_grad],
+            "weight_decay": weight_decay,
+        },
+        {
+            "params": [p for pn, p in model.named_parameters() if pn not in decay and p.requires_grad],
+            "weight_decay": 0.0,
+        },
     ]
-    optimizer = optim(optim_groups, **optim_kwargs)
+    optimizer = optim(optimizer_grouped_parameters, **optim_kwargs)
     return optimizer
